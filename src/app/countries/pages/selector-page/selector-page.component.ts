3b65edbb02bed4ae223ce49c6bef52f9054bca5a
Leo Ss <leo.vm.cba@gmail.com>
2024-11-20 09:51:51 -0300

    add: disabling the country selector until you receive a region

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CountriesService } from '../../services/countries.service';
 import { Region, SmallCountry } from '../../interfaces/country.interface';
-import { switchMap } from 'rxjs';
+import { switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector-page',
@@ -14,7 +14,7 @@ export class SelectorPageComponent implements OnInit {
 
   public myForm: FormGroup = this.fb.group({
     region: ['', Validators.required],
-    country: ['', Validators.required],
+    country: [{ value: '', disabled: true }, Validators.required],
     borders: ['', Validators.required],
   })
 
@@ -34,10 +34,20 @@ export class SelectorPageComponent implements OnInit {
   onRegionChanged(): void {
     this.myForm.get('region')!.valueChanges
     .pipe(
+      //tap( () => this.myForm.get('country')!.setValue('') ),
+      tap( () => {
+        this.myForm.get('country')!.reset({ value: '', disabled: true });
+        this.countriesByRegion = [];
+      }),
       switchMap( region => this.countriesService.getCountriesByRegion(region) )
     )
     .subscribe(countries => {
       this.countriesByRegion = countries;
+
+      if (countries.length > 0) {
+        // Habilitar el select de `country` si hay países
+        this.myForm.get('country')!.enable();
+      }
     })
   }
 }
